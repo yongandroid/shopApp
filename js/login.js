de0c92d536aab6469a9c5a62e5e98f7a90d19738
mrxf <zhshy123@foxmail.com>
2016-06-25 21:18:14 +0800

    完善登录页面

---
+++ js/login.js
@@ -0,0 +1,60 @@
+//是否记住密码自动登录
+var isRememberPass = false;
+//点击显示密码之后，显示密码
+$('.passToggler').on('touchstart',function(){
+	//改变选择框的对号显示隐藏
+	$("#tgPassCheckbox").toggleClass("unchecked");
+
+	//改变输入框的样式
+	var _passType = $("input.password").attr("type")=="text"?"password":"text";
+	$("input.password").attr({"type":_passType});
+});
+
+//当点击记住密码切换的时候
+$(".rememberToggler").on('touchstart',function(){
+	$("#rmbPassCheckbox").toggleClass("unchecked");
+
+	//改变记住密码全局变量
+	isRememberPass = isRememberPass?false:true;
+})
+
+//验证用户名和密码是否为空
+function isEmpty(_this){
+    if($(_this).val().length == 0){
+        $('span.formTip').addClass('error');
+        return true;
+    }
+    $('span.formTip').removeClass('error');
+    return false;
+}
+
+//当某个input框失去焦点的时候就验证触发事件验证文本框是否为空
+$('input').on('blur',function(){
+    isEmpty(this);
+});
+
+//当点击登录按钮之后的事件
+$('span.login-btn').on('touchstart',function(){
+	var _countEmpty = 0;
+	$('input').each(function(){
+		_countEmpty += isEmpty(this);
+	})
+	!_countEmpty && sendLoginInfo($('.username').val(),$('.password').val());
+})
+
+//发送登录信息
+function sendLoginInfo(_userId,_password){
+	$.ajax({
+        type: 'post',
+        url: "http://datainfo.duapp.com/shopdata/userinfo.php",
+        data: {
+            status: 'login',
+            userID: _userId,
+            password: _password
+        },
+        dataType: 'JSON',
+        success: function (data) {
+            console.log(JSON.parse(data));
+        }
+    })
+}
